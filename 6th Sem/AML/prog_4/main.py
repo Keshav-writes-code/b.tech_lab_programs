06016e984b79e5adb42e3b493ba3efc7349317ea
keshav-writes-code <dhimankeshav201@gmail.com>
2025-03-28 11:16:11 +0530

    add todo comments

--- 6th Sem/AML/prog_4/main.py
+++ 6th Sem/AML/prog_4/main.py
@@ -1,4 +1,15 @@
 import numpy as np
+import math
+
+## TODO: Build AN XOR Program
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(math.e, -x))
+
+
+def signmoidDerivative(x):
+    return x
 
 
 def main():
