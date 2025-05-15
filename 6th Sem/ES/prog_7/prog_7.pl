34f748eaedef81ef02d8ee4678068a64535dd645
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-05-15 11:06:17 +0530

    feat: update prog & of Prolog

--- 6th Sem/ES/prog_7/prog_7.pl
+++ 6th Sem/ES/prog_7/prog_7.pl
@@ -1,21 +1,21 @@
-queens(N,Queens) :-
-  length(Queens, N),
-  board(Queens, Board, 0, N, _, _),
-  queens(Board, 0, Queens).
-board([],[],N,N,_,_).
-board([_|Queens], [Col-Vars|Board], Col0, N, [_|VR],VC) :-
-  Col is Col0 +1,
-  functor(Vars, f, N),
-  constraints(N, Vars, VR, VC),
-  board(Queens, Board, Col, N, VR, [_|VC]).
-constraints(0,_,_,_) :- !.
-constraints(N, Row, [R|Rs], [C|Cs]) :-
-  arg(N, Row, R-C),
-  M is N -1,
-  constraints(M, Row, Rs, Cs).
-queens([],_,[]).
-queens([C|Cs], Row0, [Col|Solution]) :-
-  Row is Row0 +1,
-  select(Col-Vars, [C|Cs], Board),
-  arg(Row, Vars, Row-Row),
-  queens(Board, Row, Solution).
+queens(Solution) :-
+    Solution = [Q1, Q2, Q3, Q4],
+    permutation([1, 2, 3, 4], Solution),
+    safe(Solution).
+
+safe([]).
+safe([Q|Others]) :-
+    safe(Q, Others, 1),
+    safe(Others).
+
+safe(_, [], _).
+safe(Q, [Q1|Others], D) :-
+    Q =\= Q1,                % not in same column
+    abs(Q - Q1) =\= D,       % not in same diagonal
+    D1 is D + 1,
+    safe(Q, Others, D1).
+
+permutation([], []).
+permutation(List, [H|Perm]) :-
+    select(H, List, Rest),
+    permutation(Rest, Perm).
